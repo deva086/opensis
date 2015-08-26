f77ae42f145fecd03422a419c13c9947007f3c45
deva086 <deva086@gmail.com>
2015-08-26 07:29:21 +0600

    First commit

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysql'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = 'Nopass1234'; 
+$DatabaseName = 'opensisce'; 
+$DatabasePort = '3306'; 
+?>
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php include_once 'languages.php'; ?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title><?php echo _("Subject");?></title>
+</head>
+<body>
+    <center>
+    <h1><?php echo _("prothom-alo"); ?></h1>
+    <p><?php echo _("newspaper"); ?></p>
+    <p><?php echo _("Subject");?></p>
+    </center>
+</body>
+</html>
